69912a382efc22a14b66fcaf872d1dc73dcd436f
ari <aricerrahyan@gmail.com>
2023-07-12 14:57:25 -0400

    boiler spring application with dependecies created

---
+++ src/test/java/com/meplus/meplusartifact/MeplusartifactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meplus.meplusartifact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeplusartifactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
